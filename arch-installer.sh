5a12f3e8d091c3730fa6f6a5eff1a76489127071
Jonas <pankratz.jonas95@gmail.com>
2024-06-13 10:04:20 +0200

    updated

--- arch-installer.sh
+++ arch-installer.sh
@@ -4,15 +4,10 @@ bootoptions="mitigations=off nmi_watchdog=0 i915.enable_guc=2 i915.enable_fbc=1 
 
 
 install="
-	archlinux-keyring arch-gaming-meta base base-devel blueman bluez-utils bottles bridge-utils brother-hl2030 btop coolercontrol cups discord discord-screenaudio dnsmasq dunst fastfetch feh file-roller firefox gamemode gamescope-nvidia gedit ghostscript git gvfs hyprland hyprshot icaclient inxi iwd jdownloader2 kitty kvantum libva-nvidia-driver libva-mesa-driver linux linux-firmware linux-headers lxappearance network-manager-applet noto-fonts noto-fonts-cjk noto-fonts-emoji os-prober otf-font-awesome pamixer papirus-icon-theme pavucontrol pipewire pipewire-alsa pipewire-jack pipewire-pulse pokemon-colorscripts-git polkit-kde-agent protonplus protontricks rofi-games rofi-wayland sddm sshfs swww teams-for-linux thunar thunar-archive-plugin ttf-font-awesome ttf-iosevka-nerd ttf-jetbrains-mono tumbler udiskie unrar unzip vulkan-tools waybar wget wireplumber wlogout xdg-desktop-portal-gtk xdg-desktop-portal-hyprland xdg-utils yay-git zathura zathura-pdf-mupdf zoxide zsh zsh-autosuggestions zsh-syntax-highlighting zsh-theme-powerlevel10k-git
+	archlinux-keyring arch-gaming-meta base base-devel blueman bluez-utils  bridge-utils  btop  cups discord  dnsmasq dunst fastfetch feh  firefox gamemode  gedit ghostscript gvfs hyprland inxi iwd  kitty kvantum libva-nvidia-driver libva-mesa-driver linux linux-firmware linux-headers lxappearance network-manager-applet noto-fonts noto-fonts-cjk noto-fonts-emoji os-prober otf-font-awesome pamixer papirus-icon-theme pavucontrol   polkit-kde-agent    rofi-wayland sddm sshfs thunar thunar-archive-plugin ttf-font-awesome ttf-iosevka-nerd ttf-jetbrains-mono tumbler udiskie unrar unzip vulkan-tools waybar wget   xarchiver xdg-desktop-portal-gtk xdg-desktop-portal-hyprland xdg-utils zathura zathura-pdf-mupdf zoxide zsh zsh-autosuggestions zsh-syntax-highlighting
 "
 aurinstall="
-	https://aur.archlinux.org/aic94xx-firmware.git
-	https://aur.archlinux.org/wd719x-firmware.git
-	https://aur.archlinux.org/upd72020x-fw.git
-	https://aur.archlinux.org/ast-firmware.git
-	https://aur.archlinux.org/wireless-regdb-pentest.git
-	https://aur.archlinux.org/betaflight-configurator-bin.git
+	bottles brother-hl2030 coolercontrol discord-screenaudio gamescope-nvidia hyprshot icaclient  jdownloader2 pokemon-colorscripts-git protonplus protontricks rofi-games swww teams-for-linux wlogout zsh-theme-powerlevel10k-git
 "
 gitdownload="
 	https://gitlab.com/Suphi/Scripts.git
@@ -21,14 +16,23 @@ gitdownload="
 
 for item in ${install}; do
 		name=${item}
-		echo "Installing ${name}"
- #       sudo pacman -Syu ${name}
+		echo "\nInstalling ${name}"
+		sudo pacman -Syu ${name} --noconfirm
 	done
 
-if [ ! -f /home/jonas/Documents/nvidia-all/PKGBUILD ]; then
+if [ ! -f ~/Documents/yay/PKGBUILD ]; then
     mkdir -p ~/Documents/
     cd ~/Documents/
+    git clone https://aur.archlinux.org/yay.git
+    cd yay
+    makepkg -si
+fi
+
+if [ ! -f ~/Documents/nvidia-all/PKGBUILD ]; then
+    cd ~/Documents/
     git clone https://github.com/Frogging-Family/nvidia-all
+    cd nvidia-all/
+    makepkg -si
 fi
 
 
